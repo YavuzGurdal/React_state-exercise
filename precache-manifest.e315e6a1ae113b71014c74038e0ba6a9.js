aabd3ec39ba3588f7966a33b00d8427bafe869e7
Yavuzgrdl <32365733+Yavuzgrdl@users.noreply.github.com>
2020-03-16 17:35:41 +0100

    Updates

---
+++ precache-manifest.e315e6a1ae113b71014c74038e0ba6a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f3bb3015fd373bf655be4c711f8513f",
+    "url": "/state_exercise/index.html"
+  },
+  {
+    "revision": "9ffad4e93e1ed0ed2c0c",
+    "url": "/state_exercise/static/css/main.4f584c0a.chunk.css"
+  },
+  {
+    "revision": "3274bf1cce2ede1b281b",
+    "url": "/state_exercise/static/js/2.8b76d7b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/state_exercise/static/js/2.8b76d7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ffad4e93e1ed0ed2c0c",
+    "url": "/state_exercise/static/js/main.12d115a2.chunk.js"
+  },
+  {
+    "revision": "3807292e3a00ebdacc89",
+    "url": "/state_exercise/static/js/runtime-main.f61dbb14.js"
+  }
+]);
